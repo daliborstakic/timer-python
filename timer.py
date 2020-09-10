5c1a844067e71709c73b2cf5d73fda86909cb773
daliborstakic <stakicdalibor64@gmail.com>
2020-09-10 14:24:17 +0200

    added main window

---
+++ timer.py
@@ -0,0 +1,12 @@
+""" Importing time and Tkinter """
+import time
+import tkinter as tk
+""" Showing message windows """
+from tkinter import messagebox
+
+# Main window
+root = tk.Tk()
+root.title("Timer")
+
+if __name__ == "__main__":
+    root.mainloop()
