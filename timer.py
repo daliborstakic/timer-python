ec08ba6a0ef34d260f60a1521e27f3975b283d9f
daliborstakic <stakicdalibor64@gmail.com>
2020-09-10 14:31:43 +0200

    added first row

--- timer.py
+++ timer.py
@@ -2,11 +2,18 @@
 import time
 import tkinter as tk
 """ Showing message windows """
-from tkinter import messagebox
+from tkinter import Entry, Label, messagebox
 
 # Main window
 root = tk.Tk()
 root.title("Timer")
+
+# First row
+minutes_label = Label(root, text="Minutes:")
+minutes_entry = Entry(root)
+
+minutes_label.grid(row=0, column=0, padx=5, pady=5)
+minutes_entry.grid(row=0, column=1, pady=5, padx=5)
 
 if __name__ == "__main__":
     root.mainloop()
